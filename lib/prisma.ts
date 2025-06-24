0da8abddf4dbdf0c8e2bf10e46db0c6a423ba456
Dean Gilmore <dean@videoremix.io>
2025-06-24 18:56:49 +0530

    Updated prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,11 @@
 import { PrismaClient } from '@prisma/client'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
 declare global {
   // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined
 }
 
-export const client = globalThis.prisma || new PrismaClient()
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = client
+export const client = globalThis.prisma || new PrismaClient().$extends(withAccelerate())
+
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = client as any
